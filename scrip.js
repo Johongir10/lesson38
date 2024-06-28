a7ea4a003b29f79957d365fb73c9c1eb355f2755
Johongir10 <shavkatovj73@gmail.com>
2024-06-28 10:28:53 +0500

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,12 @@
+let  ism = prompt("String type da malumot Kiriting");
+let  ism2 = prompt("Number type da malumot Kiriting");
+let  ism3 = prompt("Object type da malumot Kiriting")
+
+
+
+confirm("Hamma malumot togrimi????????????");
+
+
+
+
+alert("Siz kiritgan " + ism + " String type da  \n  Siz kiritgan " + ism2 + " Number type da kiriitilgan  \n  Object type da " + ism3 + " Object type da  ")
